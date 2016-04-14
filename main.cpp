19f24129bed4407c6ec52e3887837756c11b6302
student <student@debian>
2016-04-14 19:42:28 +0200

    zadanie

--- main.cpp
+++ main.cpp
@@ -1,23 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <iostream>
+
+using namespace std;
 
 char **charSquare(int n);
 void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
-	drawCharSquare(charSquare(n), n);	
+	int n = atoi(argv[1]);
+	time_t valueSeed = atoi(argv[2]);
+	time_t *wskValueSeed = &valueSeed;
+	srand(time((argc == 3) ? wskValueSeed : NULL));
+	drawCharSquare(charSquare(n), n);
 }
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square = new char*[n];
+	for (int i = 0; i < n; i++)
+	{
+		square[i] = new char[n];
+
+		for (int j = 0; j < n; j++)
+		{
+			square[i][j] = rand() % ('a' - 'z' + 1) + 'a';
+		}
+	}
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
-
+	for (int i = 0; i < n; i++)
+	{
+		cout << endl;
+		for (int j = 0; j < n; j++)
+		{
+			printf("%c ", square[i][j]);
+		}
+	}
 }
